9c30c5f1e4e777ac8aac9ea7f47601e325453278
Elliot Winkler <elliot.winkler@gmail.com>
2012-05-21 22:00:47 -0600

    Add schema file

---
+++ db/schema.rb
@@ -0,0 +1,34 @@
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended to check this file into your version control system.
+
+ActiveRecord::Schema.define(:version => 20120519050914) do
+
+  create_table "reviewables", :force => true do |t|
+    t.string   "name",                                                               :null => false
+    t.string   "full_name",                                                          :null => false
+    t.string   "webkey",                                                             :null => false
+    t.datetime "created_at",                                                         :null => false
+    t.datetime "updated_at",                                                         :null => false
+    t.string   "summary"
+    t.string   "specs",              :default => "--- {}\n"
+    t.integer  "num_reviews"
+    t.string   "content_urls",       :default => "--- !ruby/object:Set\nhash: {}\n"
+    t.string   "official_urls",      :default => "--- !ruby/object:Set\nhash: {}\n"
+    t.string   "mention_urls",       :default => "--- !ruby/object:Set\nhash: {}\n"
+    t.date     "market_released_on"
+    t.date     "Date"
+    t.float    "aggregated_score"
+    t.boolean  "is_chipset"
+  end
+
+end
