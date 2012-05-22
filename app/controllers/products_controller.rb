3b8578a2e6eadd51a01fbae78c1a370011f3cfcc
Elliot Winkler <elliot.winkler@gmail.com>
2012-05-21 22:00:16 -0600

    Update ProductsController in the new FocusedController style

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,56 +1,41 @@
 
 class ProductsController < ApplicationController
-  class Action < ApplicationController
-    include FocusedController::Mixin
-  end
-
-  class Index < Action
-    SORT_RULES = {
-      "full_name" => "p.full_name.downcase",
-      "price" => "p.price",
-      "rating_index" => "(p.rating.try(:value) || 0), p.num_reviews"
-    }
-
-    def run
+  def_action :index do
+    def call
       session[:sort_key] = sort_key
       session[:sort_order] = sort_order
-      @products = Product.all
       respond_to do |format|
         format.html
         format.json { render :json => products.map(&:as_json) }
       end
     end
 
-    def sort_key
-      @sort_key ||= params[:sort_key] || session[:sort_key] || "full_name"
-    end
+    let(:sort_key) { params[:sort_key] || session[:sort_key] || "full_name" }
 
-    def sort_order
-      @sort_order ||= params[:sort_order] || session[:sort_order] || "asc"
-    end
+    let(:sort_order) { params[:sort_order] || session[:sort_order] || "asc" }
 
-    def products
-      @products ||= begin
-        products = Product.all.to_a
-        # Schwartzian transform
-        sort_rule = SORT_RULES[sort_key] or raise "Unknown sort key '#{sort_key}'"
-        tmp = eval <<-RUBY
-          products.map do |p|
-            m = manufacturers_by_id[p.manufacturer_id]
-            [p, [m.name.downcase, #{sort_rule}]]
-          end
-        RUBY
-        case sort_order
-          when "desc" then tmp.sort! {|a,b| b[1] <=> a[1] }
-          else             tmp.sort! {|a,b| a[1] <=> b[1] }
+    let(:products) {
+      products = Product.all.to_a
+      # Schwartzian transform
+      sort_rule = _sort_rules[sort_key] or raise "Unknown sort key '#{sort_key}'"
+      tmp = eval <<-RUBY
+        products.map do |p|
+          m = manufacturers_by_id[p.manufacturer_id]
+          [p, [m.name.downcase, #{sort_rule}]]
         end
-        tmp.map {|x| x[0] }
+      RUBY
+      case sort_order
+        when "desc" then tmp.sort! {|a,b| b[1] <=> a[1] }
+        else             tmp.sort! {|a,b| a[1] <=> b[1] }
       end
-    end
+      tmp.map {|x| x[0] }
+    }
 
-    def manufacturers
+    let(:manufacturers) {
       Manufacturer.sort(:name).all
-    end
+    }
+
+    # don't know if we need this or not:
 
     def products_by_manufacturer
       products.inject({}) {|h,p| m = manufacturers_by_id[p.manufacturer_id]; (h[m] ||= []) << p; h }
@@ -59,12 +44,21 @@ class ProductsController < ApplicationController
     def manufacturers_by_id
       manufacturers.inject({}) {|h,m| h[m.id] = m; h }
     end
+
+    def _sort_rules
+      return {
+        "full_name" => "p.full_name.downcase",
+        "price" => "p.price",
+        "rating_index" => "(p.rating.try(:value) || 0), p.num_reviews"
+      }
+    end
+
   end
 
-  class Show < Action
-    expose(:product) { Product.find_by_webkey(params[:id]) }
+  def_action :show do
+    let(:product) { Product.find_by_webkey(params[:id]) }
 
-    def run
+    def call
       respond_to do |format|
         format.html
         format.json { render :json => product }
