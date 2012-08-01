5e98433b1cd7e441b4a1519a3a568aa0ebf1375e
Elliot Winkler <elliot.winkler@gmail.com>
2012-07-31 23:07:38 -0600

    Fix the mutex code using Ruby's MonitorMixin

--- lib/hardwarepedia/workspace.rb
+++ lib/hardwarepedia/workspace.rb
@@ -1,90 +1,73 @@
 
-require 'thread'
+require 'monitor'
 
 module Hardwarepedia
   module Workspace
-    module Mixin
-      def mutex(klass, *keys, &block)
-        Workspace.mutex(klass, *keys, &block)
-      end
-    end
+    extend MonitorMixin
 
     class << self
-      def _mutex
-        @@mutex ||= Mutex.new
-      end
-
       def locks
-        @@locks ||= {}
+        @locks ||= {}
       end
 
-      def queue_sizes
-        @@queue_sizes ||= {}
+      def queues
+        @queues ||= {}
       end
 
       def mutex(klass, *keys, &block)
         key = [klass] + keys
-        if t = lock_for(key)
-          if t == Thread.current
-            raise "You have already locked this key: #{key.inspect}"
-          end
-          wait_for(key) { lock(key, &block) }
-        else
-          lock(key, &block)
-        end
+        wait_for(key) if lock_for(key)
+        lock(key, &block)
       end
 
       def lock_for(key)
-        _mutex.synchronize { locks[key] }
+        # if two threads read this at the exact same time, then both may read
+        # that a lock is available
+        synchronize do
+          locks[key]
+        end
       end
 
       def lock(key)
-        # FIXME: Seems like the mutex isn't quite working... two threads are
-        # attempting to reserve at the exact same time and succeeding?!
-        logger.debug "Lock reserve: #{key.inspect} (#{queue_size_for(key)} in queue)"
-        locks[key] = Thread.current
-        yield
-      ensure
-        logger.debug "Lock release: #{key.inspect} (#{queue_size_for(key)} in queue)"
-        locks.delete(key)
+        # if two threads lock at the exact same time, well, that's silly
+        synchronize do
+          begin
+            logger.debug "Lock reserve: #{key.inspect}"
+            locks[key] = Thread.current
+            yield
+          ensure
+            logger.debug "Lock release: #{key.inspect}"
+            locks.delete(key)
+            notify(key)
+          end
+        end
       end
 
       def wait_for(key)
-        increment_queue_for(key)
-        logger.debug "Lock wait: #{key.inspect} (#{queue_size_for(key)} in queue)"
-        t = Time.now
-        expires_after = 2 # seconds
-        begin
-          if (Time.now - t) > expires_after
-            raise "I've been waiting #{expires_after} seconds for #{key.inspect}. I'm giving up."
-          end
-          sleep 0.2
-        end while lock_for(key)
-        decrement_queue_for(key)
-        yield
+        logger.debug "Lock wait: #{key.inspect}"
+        queue = queues[key] ||= []
+        cond = new_cond
+        queue << cond
+        # put this thread to sleep, we will wake it up when there is an opening
+        cond.wait_while { lock_for(key) }
+      end
+
+      def notify(key)
+        if queue = queues[key]
+          # do not notify all in line, just the first one - after he finishes
+          # then he will call notify() again
+          cond = queue.unshift
+          cond.signal
+        end
       end
 
       def check_locks
         if locks.any?
-          msg = "There are still locks open for these keys:\n"
-          locks.each_key do |key|
-            msg << " * #{key.inspect}\n"
-          end
-          raise msg
+          raise "Not all locks were executed"
         end
-      end
-
-      def queue_size_for(key)
-        queue_sizes[key] ||= 0
-      end
-
-      def increment_queue_for(key)
-        queue_sizes[key] ||= 0
-        queue_sizes[key] += 1
-      end
-
-      def decrement_queue_for(key)
-        queue_sizes[key] -= 1
+        if queues.any? {|key, queue| queue.any? }
+          raise "People are still in line"
+        end
       end
     end
   end
